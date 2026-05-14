2a6c9977775589a4d9d3ebbe49b7a73de58c044b
Andrei Ungureanu <andrei.reanu@gmail.com>
2026-05-14 19:52:48 +0300

    remove unused register route

--- src/router.tsx
+++ src/router.tsx
@@ -1,6 +1,5 @@
 import { createBrowserRouter, Navigate } from 'react-router-dom'
 import Login from './pages/Login'
-import Register from './pages/Register'
 import Dashboard from './pages/Dashboard'
 import Pay from './pages/Pay'
 import ProtectedRoute from './components/ProtectedRoute'
@@ -8,8 +7,7 @@ import ProtectedRoute from './components/ProtectedRoute'
 export const router = createBrowserRouter([
   { path: '/', element: <Navigate to="/login" replace /> },
   { path: '/login', element: <Login /> },
-  { path: '/register', element: <Register /> },
-  { path: '/pay/:offerId', element: <Pay /> },
+{ path: '/pay/:offerId', element: <Pay /> },
   {
     path: '/dashboard',
     element: (
